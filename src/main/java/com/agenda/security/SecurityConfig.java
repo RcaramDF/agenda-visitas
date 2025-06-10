1d6eb45822babaf671c0c5bd33b6a3e8595df239
Ricardo Itacarambi <ricardoitacarambi@MacBook-Air-de-Ricardo.local>
2025-06-09 22:42:57 -0300

    Resolve conflito de rota / duplicada

--- src/main/java/com/agenda/security/SecurityConfig.java
+++ src/main/java/com/agenda/security/SecurityConfig.java
@@ -1,22 +1,21 @@
-// package com.agenda.security;
-//
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-// import org.springframework.security.web.SecurityFilterChain;
-//
-// @Configuration
-// public class SecurityConfig {
-//
-//     @Bean
-//     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//         http
-//             .authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll())
-//             .csrf(csrf -> csrf.disable())
-//             .headers(headers -> headers.frameOptions().disable())
-//             .formLogin(form -> form.disable())
-//             .httpBasic(httpBasic -> httpBasic.disable());
-//
-//         return http.build();
-//     }
-// }
+package com.agenda.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll())
+            .csrf(csrf -> csrf.disable())
+            .headers(headers -> headers.frameOptions().disable())
+            .formLogin(form -> form.disable())
+            .httpBasic(http -> http.disable());
+
+        return http.build();
+    }
+}
