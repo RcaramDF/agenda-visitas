26d0bcd2ece7e774e625cb506d384169a75862f6
Ricardo Itacarambi <ricardoitacarambi@MacBook-Air-de-Ricardo.local>
2025-06-09 22:38:56 -0300

    Resolve conflito de rota / duplicada

--- src/main/java/com/agenda/controller/HelloController.java
+++ src/main/java/com/agenda/controller/HelloController.java
@@ -1,13 +1,4 @@
-package com.agenda.controller;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class HelloController {
-
-    @GetMapping("/")
-    public String hello() {
-        return "Agenda de Visitas está funcionando!";
-    }
-}
+// @GetMapping("/")
+// public String hello() {
+//     return "alguma-coisa";
+// }
